83ee06d7f02e4b884980ebbafab900aba3e89812
khh-Niloy <niloy.dev.101@gmail.com>
2024-12-22 23:51:54 +0600

    added search func

--- src/Pages/AllFood.jsx
+++ src/Pages/AllFood.jsx
@@ -6,18 +6,15 @@ import { AuthContext } from "../Context/AuthContextProvider";
 const AllFood = () => {
   const [allFood, setallFood] = useState([]);
   const { user } = useContext(AuthContext);
+  const [searchText, setsearchText] = useState("");
 
   useEffect(() => {
-    fetchAllFoodData();
-  }, []);
-
-  const fetchAllFoodData = () => {
-    axios.get("https://madchef-server-side.vercel.app/allfood").then((data) => {
-      setallFood(data.data);
-    });
-  };
-
-  // console.log(allFood)
+    axios
+      .get(`http://localhost:3000/allfood?search=${searchText}`)
+      .then((data) => {
+        setallFood(data.data);
+      });
+  }, [searchText]);
 
   return (
     <div>
@@ -25,7 +22,32 @@ const AllFood = () => {
         <h1></h1>
       </div>
 
-      <div className="w-[80%] mx-auto py-20 grid grid-cols-4 gap-5">
+      <div className="w-[30%] mx-auto mt-8">
+        <label className="input input-bordered flex items-center gap-2">
+          <input
+            type="text"
+            onChange={(e) => {
+              setsearchText(e.target.value);
+            }}
+            className="grow"
+            placeholder="Search"
+          />
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 16 16"
+            fill="currentColor"
+            className="h-4 w-4 opacity-70"
+          >
+            <path
+              fillRule="evenodd"
+              d="M9.965 11.026a5 5 0 1 1 1.06-1.06l2.755 2.754a.75.75 0 1 1-1.06 1.06l-2.755-2.754ZM10.5 7a3.5 3.5 0 1 1-7 0 3.5 3.5 0 0 1 7 0Z"
+              clipRule="evenodd"
+            />
+          </svg>
+        </label>
+      </div>
+
+      <div className="w-[80%] mx-auto pb-20 pt-10 grid grid-cols-4 gap-5">
         {allFood.map((e) => (
           <div key={e._id} className="card card-compact bg-base-100 shadow-xl">
             <figure>
@@ -33,10 +55,14 @@ const AllFood = () => {
             </figure>
             <div key={e._id} className="card-body">
               <h2 className="card-title">{e.foodname}</h2>
-              <p>If a dog chews shoes whose shoes does he choose?</p>
+              <p>Price: ${e.price}</p>
+              <p>Origin: {e.origin}</p>
+              <p className="line-clamp-1">{e.description}</p>
               <div className="card-actions justify-end">
-                <Link to={`/allfood/foodDetailes/${e._id}`}>
-                  <button className="btn btn-primary">Buy Now</button>
+                <Link className="w-full" to={`/allfood/foodDetailes/${e._id}`}>
+                  <button className="btn btn-neutral text-white w-full mt-2">
+                    Details
+                  </button>
                 </Link>
               </div>
             </div>
--- src/Root/Root.jsx
+++ src/Root/Root.jsx
@@ -1,14 +1,34 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Outlet, useNavigation } from "react-router-dom";
 import Navbar from "../Components/Navbar";
 import Footer from "../Components/Footer";
 
 const Root = () => {
+  const navigation = useNavigation();
+  const [isLoading, setisLoading] = useState(false);
+
+  useEffect(() => {
+    if (navigation?.state === "loading") {
+      setisLoading(true);
+    } else {
+      setisLoading(false);
+    }
+  }, [navigation?.state]);
 
   return (
     <div>
       <Navbar></Navbar>
-      <Outlet></Outlet>
+
+      {isLoading ? (
+        <>
+          <div className="w-full h-screen flex items-center justify-center">
+            <span className="loading loading-spinner loading-lg"></span>
+          </div>
+        </>
+      ) : (
+        <Outlet></Outlet>
+      )}
+
       <Footer></Footer>
     </div>
   );
