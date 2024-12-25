95b221581ac8c0ce249e7698b36a5ef86b704b9f
khh-Niloy <niloy.dev.101@gmail.com>
2024-12-25 17:22:55 +0600

    added slider and customer review

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -2,6 +2,8 @@ import axios from "axios";
 import React, { useEffect, useState } from "react";
 import { Link, useNavigate, useNavigation } from "react-router-dom";
 import { useQuery } from "@tanstack/react-query";
+import Slider from "../Components/Slider";
+import Customer from "../Components/Customer";
 
 const Home = () => {
   //   const [homeData, sethomeData] = useState([]);
@@ -67,7 +69,7 @@ const Home = () => {
               Crafting Flavor Magic
             </p>
           </div>
-          <p className="text-sm mt-3 w-[80%] text-black/70">
+          <p className="text-sm mt-3 w-[80%]">
             Dive into a culinary journey where every dish tells a story, crafted
             with the freshest ingredients and a touch of innovation. Experience
             the art of flavor!
@@ -75,11 +77,30 @@ const Home = () => {
         </div>
       </div>
 
+      <div className="w-[95%] lg:w-[75%] mx-auto mt-20 mb-20">
+        <div className="text-center mb-8">
+          <h1 className="text-4xl font-semibold">Tempt Your Taste Buds</h1>
+          <p className="lg:text-sm text-xs mt-2">
+            Explore an irresistible collection of mouthwatering dishes, <br />{" "}
+            crafted with passion and bursting with flavors to delight your
+            senses
+          </p>
+        </div>
+        <Slider></Slider>
+      </div>
+
       <div className="w-[80%] mx-auto pb-20 pt-8">
-        <h1 className="text-center font-semibold text-2xl mb-10">
-          Top 6 best-selling food items{" "}
-          <span className="text-lg font-normal">(by purchase count)</span>
-        </h1>
+        <div className="mb-8 text-center">
+          <h1 className="text-center font-semibold text-4xl">
+            Top 6 best-selling food items{" "}
+            <span className="text-xs font-normal">(by purchase count)</span>
+          </h1>
+          <p className="lg:text-sm text-xs mt-2">
+            Discover the Top 6 Best-Selling Food Items That Have Captivated{" "}
+            <br />
+            Taste Buds Everywhere and Become Absolute Favorites
+          </p>
+        </div>
 
         <div className="grid grid-cols-3 gap-5 w-[80%] mx-auto">
           {filterData.map((e) => (
@@ -87,20 +108,26 @@ const Home = () => {
               key={e._id}
               className="card card-compact bg-base-100 shadow-xl"
             >
-              <figure>
-                <img src={e.photo} alt="Shoes" />
+              <figure className="h-[10rem] drop-shadow-xl relative">
+                <p className="text-[9px] left-2 bottom-2 drop-shadow-xl absolute bg-white px-3 py-1 rounded-lg">
+                  Purchase Count: {e.purchase_count}
+                </p>
+                <img
+                  className="object-cover rounded-2xl w-full h-[10rem]"
+                  src={e.photo}
+                  alt="Shoes"
+                />
               </figure>
-              <div key={e._id} className="card-body">
+              <div key={e._id} className="p-4">
                 <h2 className="card-title">{e.foodname}</h2>
-                <p>Price: ${e.price}</p>
-                <p className="text-xs">Purchase Count: {e.purchase_count}</p>
-                <p className="line-clamp-1">{e.description}</p>
+                <p className="text-sm">Price: ${e.price}</p>
+                <p className="line-clamp-2 text-sm mt-1">{e.description}</p>
                 <div className="card-actions justify-end">
                   <Link
                     className="w-full"
                     to={`/allfood/foodDetailes/${e._id}`}
                   >
-                    <button className="btn btn-neutral text-white w-full mt-2">
+                    <button className="btn hover:bg-[#FF2727] btn-neutral hover:border-none text-white w-full mt-2">
                       Details
                     </button>
                   </Link>
@@ -112,8 +139,23 @@ const Home = () => {
 
         <div className="flex items-center justify-center mt-20">
           <Link to={`/allfood`}>
-            <button className="btn btn-primary">See All</button>
+            <button className="px-5 py-2 text-white rounded-lg bg-[#FF2727] hover:bg-[#FF2727]/80">
+              See All
+            </button>
           </Link>
+        </div>
+
+        <div className="w-[80%] mx-auto pb-10">
+          <div className="flex items-center justify-center mt-28 mb-10 flex-col">
+            <h1 className="lg:text-4xl text-2xl font-semibold text-center">
+              What Our Customers Say
+            </h1>
+            <p className="text-center lg:text-sm text-xs mt-2">
+              Read the Experiences of Our Satisfied Customers and Find Out Why
+              They <br /> Trust Us for All Their Sports Equipment Needs
+            </p>
+          </div>
+          <Customer></Customer>
         </div>
       </div>
     </div>
