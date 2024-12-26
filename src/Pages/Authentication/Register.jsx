d3971c8c8e29a0983d521c6182458793a5e82078
khh-Niloy <niloy.dev.101@gmail.com>
2024-12-26 07:04:36 +0600

    responsive register page

--- src/Pages/Authentication/Register.jsx
+++ src/Pages/Authentication/Register.jsx
@@ -3,12 +3,15 @@ import { Link, useNavigate } from "react-router-dom";
 import { AuthContext } from "../../Context/AuthContextProvider";
 import { FaEye } from "react-icons/fa";
 import toast, { Toaster } from "react-hot-toast";
+import { FcGoogle } from "react-icons/fc";
+import { DarkModeContext } from "../../DarkModeProvider/DarkModeProvider";
 
 const Register = () => {
   const { createUser, profileInfo, googleSignIn } = useContext(AuthContext);
   const navigate = useNavigate();
   const [error, seterror] = useState("");
   const [isClicked, setisClicked] = useState(false);
+  const { toggleDarkMode, isDarkMode } = useContext(DarkModeContext);
 
   function handleSubmit(e) {
     e.preventDefault();
@@ -65,87 +68,118 @@ const Register = () => {
   }
 
   return (
-    <div>
-      <div className="hero bg-base-200 min-h-screen">
-        <div className="hero-content flex-col lg:flex-row-reverse">
-          <div className="text-center lg:text-left">
-            <h1 className="text-5xl font-bold">Register</h1>
+    <div
+      className={`z-10 relative bg-[url('https://img.freepik.com/free-photo/side-view-fried-meat-with-french-fries-ketchup_141793-4908.jpg?t=st=1735155958~exp=1735159558~hmac=ac21c60014a4072f8e1d91e378ef64e152b728427161ad87a667522bccdc449b&w=900')] bg-cover bg-center w-full ${
+        isDarkMode && "text-black"
+      }`}
+    >
+      <div className="w-full h-full bg-black/75 z-0 absolute"></div>
+
+      <div className="flex lg:w-[80%] w-[85%] mx-auto py-10 md:flex-row flex-col-reverse">
+        <div className="md:w-1/2 z-40 md:flex hidden">
+          <div className=" xl:h-[42.5rem] md:h-[45rem]">
+            <img
+              className="z-40 object-cover w-full xl:h-[42.5rem] md:h-[45rem] rounded-tl-2xl rounded-bl-2xl"
+              src="https://img.freepik.com/free-photo/side-view-fried-meat-with-french-fries-ketchup_141793-4908.jpg?t=st=1735155958~exp=1735159558~hmac=ac21c60014a4072f8e1d91e378ef64e152b728427161ad87a667522bccdc449b&w=900"
+              alt=""
+            />
           </div>
-          <div className="card bg-base-100 w-full max-w-sm shrink-0 shadow-2xl">
-            <form onSubmit={handleSubmit} className="card-body">
-              <div className="form-control">
-                <label className="label">
-                  <span className="label-text">Name</span>
-                </label>
-                <input
-                  type="text"
-                  placeholder="Name"
-                  name="name"
-                  className="input input-bordered"
-                  required
-                />
-              </div>
-              <div className="form-control">
-                <label className="label">
-                  <span className="label-text">Email</span>
-                </label>
-                <input
-                  type="email"
-                  placeholder="email"
-                  name="email"
-                  className="input input-bordered"
-                  required
-                />
-              </div>
-              <div className="form-control">
-                <label className="label">
-                  <span className="label-text">photo</span>
-                </label>
-                <input
-                  type="text"
-                  placeholder="photo"
-                  name="photo"
-                  className="input input-bordered"
-                  required
-                />
-              </div>
-              <div className="form-control relative">
-                <label className="label">
-                  <span className="label-text">Password</span>
-                </label>
-                <input
-                  type={isClicked ? "text" : "password"}
-                  placeholder="password"
-                  className="input input-bordered"
-                  name="password"
-                  required
-                />
-                <FaEye
-                  onClick={() => {
-                    setisClicked(!isClicked);
-                  }}
-                  className="absolute top-14 right-5"
-                ></FaEye>
-              </div>
-              <p>{error}</p>
-              <div className="form-control mt-6">
-                <button className="btn btn-primary">Register</button>
-                <Link to="/login">
-                  <button className="btn btn-primary">Login</button>
-                </Link>
-                <button
-                  onClick={() => {
-                    handleGoogle();
-                  }}
-                  className="btn btn-neutral"
-                >
-                  Google
-                </button>
-              </div>
-            </form>
+        </div>
+
+        <div className="z-10 md:w-1/2 border border-black/20 md:rounded-tr-2xl md:rounded-br-2xl md:rounded-bl-none md:rounded-tl-none rounded-2xl px-10 py-8 bg-white">
+          <div className="text-left mb-5">
+            <h1 className="text-4xl font-bold">Create Your Account </h1>
+            <p className="text-xs">Register below to get started.</p>
+          </div>
+          <form onSubmit={handleSubmit}>
+            <div className="form-control">
+              <label className="label">
+                <span className="label-text">Name</span>
+              </label>
+              <input
+                type="text"
+                placeholder="Name"
+                name="name"
+                className="input input-bordered"
+                required
+              />
+            </div>
+            <div className="form-control mt-3">
+              <label className="label">
+                <span className="label-text">Email</span>
+              </label>
+              <input
+                type="email"
+                placeholder="email"
+                name="email"
+                className="input input-bordered"
+                required
+              />
+            </div>
+            <div className="form-control mt-3">
+              <label className="label">
+                <span className="label-text">photo</span>
+              </label>
+              <input
+                type="text"
+                placeholder="photo"
+                name="photo"
+                className="input input-bordered"
+                required
+              />
+            </div>
+            <div className="form-control relative mt-3">
+              <label className="label">
+                <span className="label-text">Password</span>
+              </label>
+              <input
+                type={isClicked ? "text" : "password"}
+                placeholder="password"
+                className="input input-bordered"
+                name="password"
+                required
+              />
+              <FaEye
+                onClick={() => {
+                  setisClicked(!isClicked);
+                }}
+                className="absolute right-5 top-[3.3rem]"
+              ></FaEye>
+            </div>
+            <p>{error}</p>
+            <div className="form-control mt-6">
+              <button
+                className="border border-black rounded-xl
+              py-2.5 bg-[#E8252E] mb-3 border-none text-white font-medium hover:shadow-xl hover:scale-[1.01] duration-300"
+              >
+                Register
+              </button>
+            </div>
+          </form>
+
+          <div className="flex flex-col">
+            <button
+              onClick={() => {
+                handleGoogle();
+              }}
+              className="py-2.5 rounded-xl flex items-center justify-center border border-black/30 hover:shadow-xl hover:scale-[1.01] duration-300"
+            >
+              <FcGoogle className="text-2xl"></FcGoogle>{" "}
+              <h1 className="ml-1 text-black">Google</h1>
+            </button>
+            <div className="flex items-center mt-4 text-md gap-2">
+              <h1>Already have an account?</h1>
+              <Link to="/login">
+                <h1 className="font-bold text-[#E8252E] cursor-pointer">
+                  login
+                </h1>
+              </Link>
+            </div>
           </div>
         </div>
       </div>
+
+      <Toaster />
     </div>
   );
 };
